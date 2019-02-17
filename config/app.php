a79f17c0eacb1bbe48905713528fd34cfa99ce18
unknown <nikola94lukic@hotmail.com>
2019-02-17 18:39:53 +0100

    added seeder for submission and rate

--- config/app.php
+++ config/app.php
@@ -213,6 +213,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Captcha' => Mews\Captcha\Facades\Captcha::class
 
     ],
 
