b678d8ddfdc899e812c43937463f9266785e7d26
unknown <nikola94lukic@hotmail.com>
2019-07-20 18:25:58 +0200

    registration test

--- selenium/registration.py
+++ selenium/registration.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import xlrd
 import mysql.connector
 from mysql.connector import Error
+import re
 
 try:
 	connection = mysql.connector.connect(host='localhost',
@@ -10,24 +11,47 @@ try:
 										  user='root',
 										  password='')
 
-	if connection.is_connected():
-		cursor = connection.cursor()
+	try:
+		max_nb_row = 0
+		book = xlrd.open_workbook('plqr_val.xlsx')
 
-		try:
-			max_nb_row = 0
-			book = xlrd.open_workbook('plqr_val.xlsx')
+		for sheet in book.sheets():
+			max_nb_row = max(max_nb_row, sheet.nrows)
 
-			for sheet in book.sheets():
-				max_nb_row = max(max_nb_row, sheet.nrows)
+			for row in range(max_nb_row) :
+				for sheet in book.sheets() :
+					if row < sheet.nrows :
 
-				for row in range(max_nb_row) :
-					for sheet in book.sheets() :
-						if row < sheet.nrows :
-							name = sheet.row(row[0])
-							email = sheet.row(row[1])
-							password = sheet.row(row[2])
-							repeated_password = sheet.row(row[3])
+						if connection.is_connected():
+							cursor = connection.cursor()
 
+							value = sheet.row(row)	
+							print(value)
+
+							name = str(value[0])
+							name = name.replace('\'', '')
+							name = name.split(":")[1]
+							name = list(name)
+							print(type(name))
+
+							email = str(value[1])
+							email = email.replace('\'', '')
+							email = email.split(":")[1]
+							email = list(email)
+							print(type(email))
+							
+							password = str(value[2])
+							password = password.replace('\'', '')
+							password = password.split(":")[1]
+							password = list(password)
+							print(type(password))
+							
+							repeated_password = str(value[3])
+							repeated_password = repeated_password.replace('\'', '')
+							repeated_password = repeated_password.split(":")[1]
+							repeated_password = list(repeated_password)
+							print(type(repeated_password))
+							
 							browser = webdriver.Firefox()
 
 							browser.get('http://www.pl.abc')
@@ -50,44 +74,47 @@ try:
 
 							browser.implicitly_wait(10)
 
-						#	  assert browser.page_source.find("Your account is waiting for our administrator approval")
+							#assert browser.page_source.find("Your account is waiting for our administrator approval")
 
 							browser.quit()
 
-							cursor.execute("SELECT * from users order desc limit 1")  # select the table
+							cursor.execute("SELECT * from users order by id DESC limit 1 ")  # select the table
 							records = cursor.fetchall()
+							#print(records)
+							print(type(records))
+							print(type(records))
+							records = zip(*records)
+							values = []
+							for i in records:
+								values.append(i)
 
-							if (name != records[0]):
-								print("We have a missmatch")
+							if (name != values[1]):
+								print("Name not found")
+							else:
+								print("Name found")
+
+							if (email != values[2]):
+								print("Email not found")
 							else:
 								print("All good")
 
-							if (email != records[1]):
-								print("We have a missmatch")
+							if (password != values[3]):
+								print("Password not found")
 							else:
-								print("All good")
+								print("Password found")
 
-							if (password != records[2]):
-								print("We have a missmatch")
+							if (repeated_password != values[3]):
+								print("Password not matching")
 							else:
-								print("All good")
-
-							if (repeated_password != records[3]):
-								print("We have a missmatch")
-							else:
-								print("All good")
-
-
-							cursor.execute("SELECT * from users order desc limit 1")
-							records = cursor.fetchall()
+								print("Password confirmation is ok")
 
 							if (connection.is_connected()):
 								cursor.close()
 								connection.close()
 
-		except Error as e:
+	except Error as e:
 
-			print ("Unable to finish", e)
+		print ("Unable to finish", e)
 
 except Error as e:
 
